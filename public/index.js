6604bb8888362c492351b2ce69cf90214d905877
John-Giovenco <JohnGiovenco93@gmail.com>
2023-03-18 16:21:00 -0700

    final commit

--- public/index.js
+++ public/index.js
@@ -14,12 +14,16 @@ function getColor(stock){
 }
 
 async function main() {
+
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
-
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`,{
+        headers: { 
+            "Authorization": "apikey 2b10de491e0b42c1852e939487441d3f" 
+        }
+});
     const result = await response.json()
 
     const { GME, MSFT, DIS, BNTX } = result;
